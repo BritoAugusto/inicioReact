f1db512347479bb4038bc2ff05df7625a50c7759
Augusto Brito <britoaugusto1@gmail.com>
2024-08-19 11:01:10 -0300

    actualizamos el valor del estado numero para sumar y restar 1

--- src/components/contador.jsx
+++ src/components/contador.jsx
@@ -1,11 +1,30 @@
-const contador = () => {
+import { useState } from "react";
+const Contador = () => {
+    //aqui va la mayor parte de logica
+    const [numero, setNumero] = useState(0)
+    // let numero = 0;
+    
+    const sumar = ()=>{
+    
+    setNumero(numero +1)
+     
+     
+    }
+    const restar = ()=>{
+  
+    setNumero(numero -1)
+    
+    
+    }
+
     return (
         <section className="text-center container">
             <h2>Practica Contador</h2>
-            <p>10</p>
-            <button className="btn btn-primary">Enviar</button>
+            <p>{numero}</p>
+            <button className="btn btn-primary" onClick={sumar}>+1</button>
+            <button className="btn btn-danger" onClick={restar}>-1</button>
         </section>
     );
 };
 
-export default contador;
+export default Contador;
